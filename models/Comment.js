d6ccb1dad37510e4c8fe87a0a2dcc05da1eabd0a
Melisha <melisha.evans06@gmail.com>
2023-09-05 15:59:21 -0700

    update comment.js model

--- models/Comment.js
+++ models/Comment.js
@@ -3,6 +3,38 @@ const connection = require("../config/connection");
 
 class Comment extends Model {}
 
-Comment.init();
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize: connection,
+    freezeTableName: true,
+    modelName: "comment",
+  }
+);
 
 module.exports = Comment;
