e35357099530f84c435420e8c5e6ba91dcc9d908
Melisha <melisha.evans06@gmail.com>
2023-09-05 15:23:08 -0700

    update server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+sequelize.sync().then(() => {
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
+});
 });
